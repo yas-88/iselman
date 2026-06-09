9380ed3fbefdd649bfcef1d5a8889cbda44b5255
yas-88 <baza.a.yassir@gmail.com>
2026-06-09 13:52:10 -0800

    v2.0 - Iselman Morocco Fishing Guide

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'iselman-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/Main_Logo.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  if (e.request.url.includes('open-meteo.com')) return; // always live
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      if (res.ok) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => caches.match('/index.html')))
+  );
+});
