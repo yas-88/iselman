56d13fdf9f04fb94d0c16b1dda8071207827da52
yas-88 <baza.a.yassir@gmail.com>
2026-06-27 15:03:29 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'iselman-v2.6';
+const CACHE = 'iselman-v2.7';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/Main_Logo.png'];
 
 self.addEventListener('install', e => {
